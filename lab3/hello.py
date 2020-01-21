b0caa6c773b935ffd6f854d9607a524eae2eb6eb
Yiyang Wang <yiyang3@ualberta.ca>
2020-01-20 18:11:46 -0700

    lab3 code

---
+++ lab3/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+import cgi
+import cgitb
+import os
+cgitb.enable()
+
+print("Content-Type: text/html\n")
+print()
+#print("<!doctype html><title>Hello</title><h2>Hello World</h2>")
+print(os.environ)
