625d06589d75483c2ce7f6d64a0b364fe087e565
Yiyang Wang <yiyang3@ualberta.ca>
2020-01-29 17:17:53 -0700

    lab4 done

--- lab4/mysite/polls/admin.py
+++ lab4/mysite/polls/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
-
 from .models import Choice, Question
 
 admin.site.register(Choice)
 admin.site.register(Question)
+
+# Register your models here.
--- lab4/mysite/polls/models.py
+++ lab4/mysite/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
